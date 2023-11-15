9947b8036007cc53c98182d66d0402ba8ccd1b70
khlamaw <khlamaw@ust.hk>
2023-11-15 08:38:59 +0800

    minor change

--- app/src/main/java/com/mobileapplication/uniquestboard/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mobileapplication/uniquestboard/ui/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.mobileapplication.uniquestboard.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.mobileapplication.uniquestboard.databinding.LoginBinding
 import com.mobileapplication.uniquestboard.R
 
@@ -11,5 +13,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = LoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var lbtn : Button = binding.login;
+        var rbtn : Button = binding.register;
+        var et1 : EditText = binding.itsc;
+        var et2 : EditText = binding.password;
+
+        lbtn.setOnClickListener{
+            var itsc : String = et1.text.toString();
+            var pw : String = et2.text.toString();
+
+        }
     }
 }
