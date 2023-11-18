9933b1a8758914c59afcdc04c84a06120dad183d
khlamaw <khlamaw@ust.hk>
2023-11-18 20:17:33 +0800

    Login part done (php files will be uploaded later on)

--- app/src/main/java/com/mobileapplication/uniquestboard/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mobileapplication/uniquestboard/ui/login/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.mobileapplication.uniquestboard.ui.login
 
+import android.content.ContextParams
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.mobileapplication.uniquestboard.MainActivity
 import com.mobileapplication.uniquestboard.databinding.LoginBinding
 import com.mobileapplication.uniquestboard.R
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: LoginBinding
@@ -17,11 +27,28 @@ class LoginActivity : AppCompatActivity() {
         var rbtn : Button = binding.register;
         var et1 : EditText = binding.itsc;
         var et2 : EditText = binding.password;
-
+        var url : String = "http://192.168.1.115/android/DB_login.php";
+        var rq = Volley.newRequestQueue(this);
         lbtn.setOnClickListener{
             var itsc : String = et1.text.toString();
             var pw : String = et2.text.toString();
-
+            var sr = object : StringRequest(Request.Method.POST, url,
+                Response.Listener { response ->
+                    if(response.toString().trim() == "The record is found.")
+                    {
+                        Toast.makeText(applicationContext, "Login successes", Toast.LENGTH_SHORT).show();
+                        Intent(this, MainActivity::class.java).also { startActivity(it); }
+                    }},
+                Response.ErrorListener { e -> Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show() })
+            {
+                override fun getParams(): MutableMap<String, String>? {
+                    var params = HashMap<String, String>();
+                    params.put("itsc", itsc);
+                    params.put("password", pw);
+                    return params;
+                }
+            }
+            rq.add(sr);
         }
     }
 }
